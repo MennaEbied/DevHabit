22b0bc20961db04e8959831798b743a76aa3c94f
MennaEbied <mennahesham253@gmail.com>
2026-06-14 23:53:47 +0300

    Add FluentValidation and enhance DTO validation

--- DevHabit.Api/DevHabit.Api/Controllers/TagsController.cs
+++ DevHabit.Api/DevHabit.Api/Controllers/TagsController.cs
@@ -1,9 +1,11 @@
 ﻿using DevHabit.Api.Database;
 using DevHabit.Api.DTOs.Habits;
 using DevHabit.Api.DTOs.Tags;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using DevHabit.Api.Entities;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevHabit.Api.Controllers;
@@ -40,17 +42,39 @@ public sealed class TagsController(ApplicationDbContext dbContext) : ControllerB
         return Ok(tag);
     }
     [HttpPost]
-    public async Task<ActionResult<TagDto>> CreateTag(CreateTagDto createTagDto)
+    public async Task<ActionResult<TagDto>> CreateTag(
+        CreateTagDto createTagDto,
+        IValidator<CreateTagDto> validator,
+        ProblemDetailsFactory problemDetailsFactory)
     {
+        ValidationResult validationResult = await validator.ValidateAsync(createTagDto);
+
+        if (!validationResult.IsValid)
+        {
+            ProblemDetails problem = problemDetailsFactory.CreateProblemDetails(
+                HttpContext,
+                StatusCodes.Status400BadRequest);
+            problem.Extensions.Add("errors", validationResult.ToDictionary());
+
+            return BadRequest(problem);
+        }
+
         Tag tag = createTagDto.ToEntity();
+
         if (await dbContext.Tags.AnyAsync(t => t.Name == tag.Name))
         {
-            return Conflict($"The tag '{tag.Name}' already exists");
+            return Problem(
+                detail: $"The tag '{tag.Name}' already exists",
+                statusCode: StatusCodes.Status409Conflict);
         }
+
         dbContext.Tags.Add(tag);
+
         await dbContext.SaveChangesAsync();
+
         TagDto tagDto = tag.ToDto();
-        return CreatedAtAction(nameof(GetTag), new { id = tag.Id }, tagDto);
+
+        return CreatedAtAction(nameof(GetTag), new { id = tagDto.Id }, tagDto);
     }
     [HttpPut("{id}")]
     public async Task<ActionResult> UpdateTag(string id, UpdateTagDto updateTagDto)
